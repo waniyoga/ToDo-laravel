9873a0ce4fba2ccf59d5d3371f89694690488f61
waniyoga <130149695+waniyoga@users.noreply.github.com>
2023-06-04 17:10:25 +0900

    first commit

---
+++ app/Policies/TaskPolicy.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Policies;
+
+use Illuminate\Auth\Access\HandlesAuthorization;
+use App\Models\User;
+use App\Models\Task;
+
+class TaskPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+        /**
+        * 指定されたユーザーのタスクのとき削除可能
+        *
+        * @param User $user
+        * @param Task $task
+        * @return bool
+        */
+        public function destroy(User $user, Task $task)
+        {
+            return $user->id === $task->user_id;
+        }
+}
